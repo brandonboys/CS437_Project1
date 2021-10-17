d9edf08d376912c15822757bf2759953249cad55
enoch levandovsky <Enoch_Edge>
2021-10-17 10:45:22 -0600

    new file:   .gitignore 	new file:   QueryLogger.py 	modified:   main.py

---
+++ QueryLogger.py
@@ -0,0 +1,7 @@
+
+
+def logger(query):
+    """
+    the purpose here is to add on the querylog.txt to access a history of logs
+    You will need to check the homework on the details a log needs to cotain
+    """
--- main.py
+++ main.py
@@ -4,19 +4,30 @@ import warnings
 from FrenchFilter import frenchFilter
 from SnippetGenerator import snippetGenerator
 from synonymSuggestor import querrySuggestor
+from QueryLogger import logger
 
 warnings.filterwarnings('ignore')
 
 query = ' '.join(sys.argv[1:])
+
+#ask for a suggestion and return what the user wanted
 query = querrySuggestor(query)
 
 
-
+##get top 5 results
 out = COSINE_TD_IDF_Ranking(query)
 
 i = 0
 while i < 5:
+    #print out title
     print('Tweet ID' + str(out[0][i]))
+
+    #snip it
+    #french filter it
+    #and display
     print(frenchFilter(snippetGenerator(query,out[1][i])))
     print('\n')
     i+=1
+
+##log it
+logger(query)
