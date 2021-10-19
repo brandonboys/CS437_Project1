ff997947d32c4342a5c6ef835c275486b3af7b07
enoch levandovsky <Enoch_Edge>
2021-10-19 14:42:53 -0600

    logger update2

--- lib/QueryLogger.py
+++ lib/QueryLogger.py
@@ -10,7 +10,7 @@ def logger(query):
     the purpose here is to add on the querylog.txt to access a history of logs
     You will need to check the homework on the details a log needs to cotain
     """
-
+    df =pd.read_pickle('data/QuerryLog.pickle')
     df.append([{'AnonID':str(getpass.getuser()),'Query':str(query),'QueryTime':str(datetime.now()),'Score':0,'Appeared':0,'Used':0}],ignore_index=True)
+    df.to_pickle('data/QuerryLog.pickle')
 
-df.to_pickle('data/QuerryLog.pickle')
